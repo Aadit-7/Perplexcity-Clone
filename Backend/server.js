556de6dfa4b85a5405f465e1bac5c1a5e364f4f9
Aadit Yadav <aadityadav121@gmail.com>
2026-06-24 23:19:53 +0530

    Initialised the socket servers on both server and client side

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectToDb from "./src/config/data.database.js";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const port = process.env.PORT || 8080;
 
-connectToDb();
-app.listen(port, () => {
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
+
+connectToDb().catch((err) => {
+  console.log("MongoDB connection failed:", err);
+  process.exit(1);
+});
+
+httpServer.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
 });
--- Frontend/src/app/features/chat/pages/Dashboard.jsx
+++ Frontend/src/app/features/chat/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useChat } from "../hooks/useChat";
 
 const Dashboard = () => {
+  const chat = useChat();
   const { user } = useSelector((state) => state.auth);
 
   console.log(user);
+
+  useEffect(() => {
+    chat.initializeSocketConnetction();
+  }, []);
 
   return <div>Dashboard</div>;
 };
